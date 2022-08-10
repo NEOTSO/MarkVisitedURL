fd4661f0c664bc68e95c3605f34b3a86d0bd11fb
TSO <coding.tso@gmail.com>
2022-08-10 17:29:10 +0800

    feat: delete duplicate records

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -9,3 +9,13 @@ export function formatDate() {
     const d = prefixZero(+t.getDate(), 2);
     return y + m + d;
 };
+
+export function uniqueArray(arr: string[]): string[] {
+    const newArr: string[] = []
+    for (let i = 0; i < arr.length; i++) {
+        if (!newArr.includes(arr[i])) {
+            newArr.push(arr[i])
+        }
+    }
+    return newArr;
+}
